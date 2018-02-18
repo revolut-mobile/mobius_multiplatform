be566b406764e25f96a922af2a05ab0eb52aea1c
Roman Iatcyna <roman.yatsyna@revolut.com>
2018-02-18 23:07:23 +0300

    Added Android module

---
+++ common-jvm/src/main/kotlin/com/revolut/native/card/RevolutCard.kt
@@ -0,0 +1,13 @@
+package com.revolut.native.card
+
+actual class RevolutCardImpl(override val id: String) : RevolutCard {
+
+    override fun printIdAsync() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun runAsync(l: () -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ mobius.app/src/main/kotlin/com/example/mobiusapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.mobiusapp
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
