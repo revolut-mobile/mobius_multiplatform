7503e1d9a295bf1f76e161eb480241a518b568eb
Ivan Vazhnov <ivan.vazhnov@revolut.com>
2018-03-06 17:21:27 +0300

    upd

--- common-native/src/main/kotlin/com/revolut/domain/repositories/CardsRepository.kt
+++ common-native/src/main/kotlin/com/revolut/domain/repositories/CardsRepository.kt
@@ -14,27 +14,9 @@ actual open class CardsRepository {
 
     actual suspend fun getAllCards(): List<RevolutCard> {
         return suspendCoroutineOrReturn { continuation ->
-            /** Here any iOS async loading libray can be used,
-             *  after loading is done:
-             *  continuation.resume(List<RevolutCard>) must be called.
-             *  If any error: continuation.error(Throwable).
-             * */
-//
-//            try {
-//                val cards = getAllCardsSync()
-//                continuation.resume(cards)
-//            } catch (t: Throwable) {
-//                continuation.resumeWithException(t)
-//            }
-
             getAllCardsSync {
                 continuation.resume(it)
             }
-//            Networking.getAllCards(onSuccess -> {
-//                continuation.resume(it)
-//            }, onError-> {
-//                continuation.resumeWithException(it)
-//            })
             COROUTINE_SUSPENDED
         }
     }
