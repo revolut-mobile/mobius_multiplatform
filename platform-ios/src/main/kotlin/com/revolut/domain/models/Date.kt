7d9e25cf573316e0078955f712c9e247b052658a
Ivan Vazhnov <ivan.vazhnov@revolut.com>
2018-04-17 18:40:29 +0300

    nsdate support

--- platform-ios/src/main/kotlin/com/revolut/domain/models/Date.kt
+++ platform-ios/src/main/kotlin/com/revolut/domain/models/Date.kt
@@ -1,11 +1,21 @@
 package com.revolut.domain.models
 
+import platform.Foundation.*
+
 actual class Date actual constructor(
         private val utcMillis: Long
 ) {
 
+    private val dateTime = NSDate.dateWithTimeIntervalSince1970(utcMillis / 1000.0)
+
+    private val calendarComponents by lazy {
+        with(NSCalendar(ident = NSGregorianCalendar)) {
+            calendar.components(unitFlags = NSCalendarUnitDay, fromDate = dateTime)
+        }
+    }
+
     actual fun dayOfMonth(): Int {
-        TODO()
+        return calendarComponents.day.toInt()
     }
 
 }
