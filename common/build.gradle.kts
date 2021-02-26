c0b82b8d4475dbe6492b8f3c6b4f472e0fe358f4
aliaksei.stulnikau <aliaksei.stulnikau@revolut.com>
2021-02-26 14:19:13 +0100

    Use modern build gradle layout

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -31,13 +31,8 @@ android {
 
 kotlin {
     android()
-    val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
-        if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
-            ::iosArm64
-        else
-            ::iosX64
 
-    iOSTarget("ios") {
+    ios {
         binaries {
             framework {
                 baseName = "SharedCode"
@@ -86,7 +81,10 @@ android {
 val packForXcode by tasks.creating(Sync::class) {
     group = "build"
     val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
-    val framework = kotlin.targets.getByName<KotlinNativeTarget>("ios").binaries.getFramework(mode)
+    val sdkName = System.getenv("SDK_NAME") ?: "iphonesimulator"
+    val targetName = "ios" + if (sdkName.startsWith("iphoneos")) "Arm64" else "X64"
+    val framework =
+        kotlin.targets.getByName<KotlinNativeTarget>(targetName).binaries.getFramework(mode)
     inputs.property("mode", mode)
     dependsOn(framework.linkTask)
     val targetDir = File(buildDir, "xcode-frameworks")
